b6be47c4cebb5696919bc8d01473eebd3d0db31a
accwill <songyaqigg@gmail.com>
2021-09-11 07:20:08 +0800

    test(downloader): 添加测试用例

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,15 +5,17 @@ export enum DataType {
 
 /** 请求错误时，再次发起时间 */
 export const ReconnectionTime = 1000
+/** 默认失败请求重复次数 */
+export const ReconnectCount = 5
 
 /** 请求状态 */
 export enum Status {
   /** 加载中 */
-  Loading,
+  Loading = 'Loading',
   /** 加载成功 */
-  Success,
+  Success = 'Success',
   /** 加载失败 */
-  Error,
+  Error = 'Error',
   /** 正常 */
-  Normal
+  Normal = 'Normal'
 }
--- tests/utils/ask.spec.ts
+++ tests/utils/ask.spec.ts
@@ -1,13 +1,37 @@
-import { Ask } from '@/utils/ask'
+import { ReconnectCount, Status } from '@/constants'
+import ask, { Ask, Type } from '@/utils/ask'
+import { isString } from '@/utils/helper'
 
-it('请求测试, 重新链接次数', async () => {
-  const ask = new Ask({ url: '/', reconnectionCount: 5, timeout: 100 })
+it('ask测试, 失败请求次数', async () => {
+  const ask = new Ask({ url: 'http://www.baidu.com', reconnectCount: 2, timeout: 20 })
   await ask.start()
-  expect(ask.requestCount).toEqual(6)
+  expect(ask.requestCount).toEqual(3)
 })
 
-it('请求测试，正常连接次数', async () => {
-  const ask = new Ask({ url: 'http://www.baidu.com', reconnectionCount: 5, baseURL: '' })
+it('ask测试，成功请求次数', async () => {
+  const ask = new Ask({ url: 'http://www.baidu.com' })
   await ask.start()
   expect(ask.requestCount).toEqual(1)
+})
+
+it('ask测试，ask简单方法的使用', async () => {
+  const instance = ask('http://www.baidu.com')
+  expect(instance.resCtx).toBeInstanceOf(Promise)
+  await instance.resCtx
+  expect(isString(instance.response.data)).toEqual(true)
+})
+
+it('ask测试，ask状态', async () => {
+  const instance = new Ask({ url: 'http://www.baidu.com', type: Type.Text })
+  const askState = instance.state
+  expect(askState.status).toBe(Status.Normal)
+  expect(askState.response).toBe(null)
+  instance.start()
+  expect(askState.status).toBe(Status.Loading)
+  await instance.resCtx
+  expect(askState.requestCount).toBe(1)
+  expect(askState.requestCount).toBe(instance.requestCount)
+  expect(askState.reconnectCount).toBe(ReconnectCount)
+  expect(askState.status).toBe(Status.Success)
+  expect(isString(askState.response.data)).toBe(true)
 })
